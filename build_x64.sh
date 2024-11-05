2befafb5d903bb5d5fc8348fcf40f19c7bc61993
endernon <ender_non@outlook.com>
2024-11-05 21:03:45 +0000

    fix some typos

--- build_x64.sh
+++ build_x64.sh
@@ -1,10 +1,10 @@
-cargo build --target x86_64-unknown-linux-gnu --package idmanglercli --bin idmanglercli --release
-cargo build --target x86_64-pc-windows-gnu --package idmanglercli --bin idmanglercli --release
+cargo build --target x86_64-unknown-linux-gnu --package idmangler-cli --bin idmangler-cli --release
+cargo build --target x86_64-pc-windows-gnu --package idmangler-cli --bin idmangler-cli --release
 
 mkdir -p product
 
-cp ./target/x86_64-pc-windows-gnu/release/idmanglercli.exe ./product/idmangler-gui-windows-x64
-cp ./target/x86_64-unknown-linux-gnu/release/idmanglercli ./product/idmangler-gui-linux-x64
+cp ./target/x86_64-pc-windows-gnu/release/idmangler-cli.exe ./product/idmangler-cli-windows-x64.exe
+cp ./target/x86_64-unknown-linux-gnu/release/idmangler-cli ./product/idmangler-cli-linux-x64
 cp -u -p ./{config.json,config_hanafupookie.json,config_singu.json} ./product/
 
 cd product
