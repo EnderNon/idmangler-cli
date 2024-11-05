1747e27d7535477ff1db8686f16733c98e73841b
endernon <ender_non@outlook.com>
2024-11-05 20:39:33 +0000

    copy some relevant configs in

--- build_x64.sh
+++ build_x64.sh
@@ -5,3 +5,4 @@ mkdir -p product
 
 cp ./target/x86_64-pc-windows-gnu/release/idmanglercli.exe ./product/idmangler-gui-windows-x64
 cp ./target/x86_64-unknown-linux-gnu/release/idmanglercli ./product/idmangler-gui-linux-x64
+cp -u -p ./{config.json,config_hanafupookie.json,config_singu.json} ./product/
