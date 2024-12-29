d5927ae1256f31fec05986c6ea6261e07c2ae584
endernon <ender_non@outlook.com>
2024-12-29 20:05:30 +0000

    update build script

--- build_x64.sh
+++ build_x64.sh
@@ -3,18 +3,18 @@ cargo build --target x86_64-pc-windows-gnu --package idmangler-cli --bin idmangl
 
 mkdir -p product
 
-cp ./target/x86_64-pc-windows-gnu/release/idmangler-cli.exe ./product/idmangler-cli-windows-x64.exe
-cp ./target/x86_64-unknown-linux-gnu/release/idmangler-cli ./product/idmangler-cli-linux-x64
+cp ./target/x86_64-pc-windows-gnu/release/idmangler-cli.exe ./product/idmangler-cli.exe
+cp ./target/x86_64-unknown-linux-gnu/release/idmangler-cli ./product/idmangler-cli
 cp -u -p ./{config.json,config_hanafupookie.json,config_singu.json,config.md} ./product/
 
 pkgversion=$(cargo metadata --format-version=1 --no-deps | jq '.packages[] | select(.name == "idmangler-cli") | .version')
 echo "package version is:"
-echo $pkgversion
+echo "$pkgversion"
 balls=${pkgversion::-1}
 balls2=${balls:1}
-echo $balls2
+echo "$balls2"
 
 cd product
 
-zip "idmangler-windows-x64-${balls2}.zip" config.json config_hanafupookie.json config_singu.json config.md idmangler-cli-windows-x64.exe
-zip "idmangler-linux-x64-${balls2}.zip" config.json config_hanafupookie.json config_singu.json config.md idmangler-cli-linux-x64
+zip "idmangler-windows-x64-${balls2}.zip" config.json config_hanafupookie.json config_singu.json config.md idmangler-cli.exe
+zip "idmangler-linux-x64-${balls2}.zip" config.json config_hanafupookie.json config_singu.json config.md idmangler-cli
